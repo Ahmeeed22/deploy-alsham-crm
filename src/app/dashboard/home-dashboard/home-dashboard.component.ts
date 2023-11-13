0520d8a27dd42572e07ac6915bf2cc1d1230798b
Ahmeeed22 <ahmeeedzezo33@gmail.com>
2023-11-13 16:19:34 +0400

    add main page for owner

--- src/app/dashboard/home-dashboard/home-dashboard.component.ts
+++ src/app/dashboard/home-dashboard/home-dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CustomersService } from '../customer/customers.service';
+import { Component, OnInit } from '@angular/core';
 import { TransactionsService } from '../transactions/transactions.service';
 
 @Component({
@@ -7,133 +6,76 @@ import { TransactionsService } from '../transactions/transactions.service';
   templateUrl: './home-dashboard.component.html',
   styleUrls: ['./home-dashboard.component.scss']
 })
-export class HomeDashboardComponent implements OnInit , OnDestroy{
-  pettyCash=0;
-  detailsProfite:any={}
-  count:number=0;
-  filteration:any
-  detailsProfiteMonthly:any={}
-  countMonthly:number= 0 ;
-  amountCash:number= 0 ;
-  dateToday=new Date() ;
-  dailySummary:any ;
-  monthlySummary:any ;
+export class HomeDashboardComponent implements OnInit {
+  detailsProfite: any = {}
+  count: number = 0;
+  filteration: any
+  detailsProfiteMonthly: any = {}
+  countMonthly: number = 0;
+  amountCash: number = 0;
+  dateToday = new Date();
+  dailySummary: any;
+  monthlySummary: any;
   constructor(
-    private _TransactionsService:TransactionsService  ,
-    private _CustomersService:CustomersService
-  ) { 
+    private _TransactionsService: TransactionsService,
+  ){
     var start = new Date();
-    start.setUTCHours(0,0,0,0);
+    start.setUTCHours(0, 0, 0, 0);
     var end = new Date();
-    end.setUTCHours(23,59,59,999);
+    end.setUTCHours(23, 59, 59, 999);
 
     this.filteration = {
-      date:true ,
-      startedDate :start.toISOString(),
-      endDate : end.toISOString() ,
+      date: true,
+      startedDate: start.toISOString(),
+      endDate: end.toISOString(),
     }
   }
-  
-  
+
+
   ngOnInit(): void {
-    this.getPettyCash();
+    // getting daily and monthly summary 
     this.getAllTransactions();
-    // this.getMonthlySummary()
   }
 
-  getAllTransactions(){
-    this.filteration.offset=this.filteration.offset > 0 ? this.filteration.offset - 1 : 0 
-    // this._TransactionsService.getAllTransactions(this.filteration).subscribe({
-    //   next:(res)=>{
-    //     this.count=res.result.count
-    //     this.detailsProfite={...res.allProfite[0]} ;
-    //     console.log(res);
-        
-    //   }
-    // })
-
+  getAllTransactions() {
+    this.filteration.offset = this.filteration.offset > 0 ? this.filteration.offset - 1 : 0
     this._TransactionsService.getSummary(this.filteration).subscribe({
-      next :res=>{
-        console.log("daily",res);
-        this.dailySummary=res
-      },
-      error :err=>{
-        console.log(err);
-        
-      }
-    })
-  }
-
-   startOfMonth(date:any)
-  {
-   return new Date(date.getFullYear(), date.getMonth(),1);
-  }
-  getAllTransactionsMonthly(){
-    let dt = new Date(); 
-    // let start = this.startOfMonth(dt).toISOString();
-    let start = new Date(2019,10,10); 
-    var end = new Date();
-    end.setUTCHours(23,59,59,999);
-    this.filteration = {
-      date:true ,
-      startedDate :start,
-      endDate : end.toISOString() ,
-    }
-    this.filteration.offset=this.filteration.offset > 0 ? this.filteration.offset - 1 : 0 
-    this._TransactionsService.getAllTransactions(this.filteration).subscribe({
-      next:(res)=>{
-        this.countMonthly=res.result.count
-        this.detailsProfiteMonthly={...res.allProfite[0]}
-        this.amountCash=+this.detailsProfiteMonthly.paymentAmount +this.pettyCash - (+this.detailsProfiteMonthly.total_price_without_profite )
-      }
-    })
-    this._TransactionsService.getSummary(this.filteration).subscribe({
-      next :res=>{
-        console.log("monthly",res);
-        this.monthlySummary=res
-      },
-      error :err=>{
-        console.log(err);
-        
-      }
-    })
-  }
-  getPettyCash(){
-    this._CustomersService.getAllCustomersSearch({name:'petty Cash'}).subscribe({
-      next : (res)=>{
-        // console.log(res);
-        this.pettyCash=res.result[0]?.transactions[0]?.paymentAmount ||0 ;
+      next: res => {
+        console.log("daily", res);
+        this.dailySummary = res
         this.getAllTransactionsMonthly();
+      },
+      error: err => {
+        console.log(err);
       }
     })
   }
+  // helper to get start of month 
+  startOfMonth(date: any) {
+    return new Date(date.getFullYear(), date.getMonth(), 1);
+  }
 
-  
-  // getMonthlySummary(){
-  //   let dt = new Date(); 
-  //   let start = this.startOfMonth(dt).toISOString();
-  //   var end = new Date();
-  //   end.setUTCHours(23,59,59,999);
-  //   this.filteration = {
-  //     date:true ,
-  //     startedDate :start,
-  //     endDate : end.toISOString() ,
-  //   }
-  //   this.filteration.offset=this.filteration.offset > 0 ? this.filteration.offset - 1 : 0  ;
-  //   this._TransactionsService.getSummary(this.filteration).subscribe({
-  //     next :res=>{
-  //       console.log(res);
-  //     },
-  //     error :err=>{
-  //       console.log(err);
-        
-  //     }
-  //   })
-
-  // }
-
-  ngOnDestroy(): void {
-   
+  getAllTransactionsMonthly() {
+    let dt = new Date();
+    let start = this.startOfMonth(dt).toISOString();
+    // let start = new Date(2019,10,10); 
+    var end = new Date();
+    end.setUTCHours(23, 59, 59, 999);
+    this.filteration = {
+      date: true,
+      startedDate: start,
+      endDate: end.toISOString(),
+    }
+    this.filteration.offset = this.filteration.offset > 0 ? this.filteration.offset - 1 : 0
+    this._TransactionsService.getSummary(this.filteration).subscribe({
+      next: res => {
+        console.log("monthly", res);
+        this.monthlySummary = res
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
   }
 
 }
--- src/app/dashboard/layout/layout.component.ts
+++ src/app/dashboard/layout/layout.component.ts
@@ -3,94 +3,92 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { ReminderService } from '../reminder/reminder.service';
-import * as moment from 'moment';
+// import * as moment from 'moment';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent{
-  sideBarOpen:boolean=true ;
-  isLogged:boolean =false;
-  user:any;
-  isReminder:boolean=false ;
-  isReminderUrgent:boolean=false ;
+export class LayoutComponent {
+  sideBarOpen: boolean = true;
+  isLogged: boolean = false;
+  user: any;
+  isReminder: boolean = false;
+  isReminderUrgent: boolean = false;
   isAccountantSubMenuOpen: boolean = false;
 
-  constructor(private authService:AuthService , private router:Router, private toaster:ToastrService,private _ReminderService: ReminderService) { 
-   this.authService.currentUser.subscribe(res=> {
-      this.isLogged = this.authService.currentUser.getValue()!==null ? true : false ;
-      this.user=this.authService.currentUser.getValue()
+  constructor(private authService: AuthService, private router: Router, private toaster: ToastrService, private _ReminderService: ReminderService) {
+    this.authService.currentUser.subscribe(res => {
+      this.isLogged = this.authService.currentUser.getValue() !== null ? true : false;
+      this.user = this.authService.currentUser.getValue()
       console.log(this.user);
-      
-    }) 
-    setInterval(()=>{
+
+    })
+    setInterval(() => {
       this.hiA()
-    },6000)
+    }, 6000)
     this.getAllReminders()
-    this._ReminderService.IsReminder.subscribe(()=>{
+    
+    this._ReminderService.IsReminder.subscribe(() => {
     })
 
-    this._ReminderService.IsReminder.subscribe(()=>{
-     this.isReminder=this._ReminderService.IsReminder.getValue() ;
-     this.isReminderUrgent=this._ReminderService.IsReminderUrgent.getValue() ;
-    }) 
-  }  
-
-  toggleSidebar(){
-    this.sideBarOpen= !this.sideBarOpen
+    this._ReminderService.IsReminder.subscribe(() => {
+      this.isReminder = this._ReminderService.IsReminder.getValue();
+      this.isReminderUrgent = this._ReminderService.IsReminderUrgent.getValue();
+    })
   }
 
-  logOut(){
-    
+  toggleSidebar() {
+    this.sideBarOpen = !this.sideBarOpen
+  }
+
+  logOut() {
+
     localStorage.removeItem('token')
     this.authService.currentUser.next(null)
     this.router.navigate(['/login'])
-    this.toaster.success("logout Succesfully" , "Success")
-   }
-   hiA(){
-    var pathA:any=document.getElementById("ramd");
-     pathA?.classList?.toggle("hiA");
-     setTimeout(function(){  pathA?.classList?.toggle("hiA"); }, 3000);
-    }
+    this.toaster.success("logout Succesfully", "Success")
+  }
+  hiA() {
+    var pathA: any = document.getElementById("ramd");
+    pathA?.classList?.toggle("hiA");
+    setTimeout(function () { pathA?.classList?.toggle("hiA"); }, 3000);
+  }
 
-    getAllReminders(filter?: any) {
+  getAllReminders(filter?: any) {
 
-      this._ReminderService.getAllReminders(filter).subscribe({
-        next: (res) => {
-          // Calculate the date 4 days ago
+    this._ReminderService.getAllReminders(filter).subscribe({
+      next: (res) => {
+        // Calculate the date 4 days ago
+        const fourDaysAgo = new Date();
+        fourDaysAgo.setDate(fourDaysAgo.getDate() - 4);
+
+        const urgentRemider = res.result.rows.filter((item: any) => {
           const fourDaysAgo = new Date();
           fourDaysAgo.setDate(fourDaysAgo.getDate() - 4);
+          const dateExpire = new Date(item.dateExpire);
+          return dateExpire < fourDaysAgo;
+        })
+        console.log("urgentRemider = ", urgentRemider);
 
-          const urgentRemider=res.result.rows.filter((item:any)=>{
-            // console.log("item.dateExpire",item.dateExpire,"fourDaysAgo",fourDaysAgo);
-            // console.log(typeof moment(item.dateExpire).toDate() );
-            
-            
-            return moment(item.dateExpire).toDate() < fourDaysAgo
-          })
-          console.log(urgentRemider);
-          
-          urgentRemider.length>0 ? this._ReminderService.IsReminderUrgent.next(true) : this._ReminderService.IsReminderUrgent.next(false)
-          console.log(this._ReminderService.IsReminderUrgent.getValue());
-          
-          if (!filter&&res.result.rows.length>0) {
-            this._ReminderService.IsReminder.next(true)
-          } 
-          if(!filter&&res.result.rows.length==0){
-            this._ReminderService.IsReminder.next(false)
-          }
+        urgentRemider.length > 0 ? this._ReminderService.IsReminderUrgent.next(true) : this._ReminderService.IsReminderUrgent.next(false)
+        console.log(this._ReminderService.IsReminderUrgent.getValue());
 
-         
+        if (!filter && res.result.rows.length > 0) {
+          this._ReminderService.IsReminder.next(true)
         }
-      })
-      
-    }
-    toggleAccountantSubMenu() {
-      console.log("test");
-      
-      this.isAccountantSubMenuOpen = !this.isAccountantSubMenuOpen;
-    }
-    
+        if (!filter && res.result.rows.length == 0) {
+          this._ReminderService.IsReminder.next(false)
+        }
+
+
+      }
+    })
+
+  }
+  toggleAccountantSubMenu() {
+    this.isAccountantSubMenuOpen = !this.isAccountantSubMenuOpen;
+  }
+
 }
